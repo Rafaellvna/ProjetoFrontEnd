20f251b62bea7057080c6dcad555c75e66804727
Rafael <Rafael.viana@iesb.edu.br>
2023-04-11 22:14:00 -0300

    Atualizando o Projeto FrontEnd

--- services/apiFilmes.js
+++ services/apiFilmes.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const apiFilmes = axios.create({
     baseURL: 'https://api.themoviedb.org/3',
     headers: { 
-        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3NDc1OTk1NTU0ZjlkMDBkYTU0YTcxZTBhNGYzMzRlYSIsInN1YiI6IjY0MzVlNTE0ZmQ0ZjgwMDBiNGEyZDM0NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.NGToJHCFrL4xbyYUkciWIlIyNnLuCczXc9p8aVPOTcc'
+        Authorization: ''
     }
 })
 
